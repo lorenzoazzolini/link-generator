3c5d6fa1e2a2c324e8f5643acfb519e325db48c6
Jamie Draper <jamiedrpr@gmail.com>
2019-08-02 16:58:46 +0100

    add jquery, add copy to clipboard

--- main.js
+++ main.js
@@ -1,7 +1,19 @@
 $(function () {
     $('.btn-whatsapp').on('click', function () {
-        var urlBase = 'https://api.whatsapp.com/send?text=',
-            shareText = $('textarea.input').val();
-        $('p.output').html(urlBase + encodeURIComponent(shareText));
+        var urlBase = 'https://wa.me/?text=',
+            shareText = $('#input').val();
+        $('#output').html(urlBase + encodeURIComponent(shareText));
+
+        var copyText = document.getElementById("output");
+        copyText.select();
+        document.execCommand("copy");
+    });
+    $('.btn-whatsapp').on('mouseenter', function(){
+        console.log('foo')
+        $("#myTooltip").html('Copy to clipboard');
+    });
+    $('.btn-whatsapp').on('mouseup', function(){
+        $("#myTooltip").html('Copied to clipboard!');
     });
 });
+
